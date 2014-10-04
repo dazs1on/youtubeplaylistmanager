3c4618f07cb74879b9eb1aa833aaaa69c6043f66
Dazs1on <ahoskinson93@gmail.com>
2014-10-04 18:40:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,78 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+var express = require('express'),
+	app = express(),
+	http = require('http'),
+	path = require('path'),
+	mongoose = require('mongoose'),
+	hash = require('./pass').hash;
 
-server.listen(8080);
+mongoose.connect('mongodb://localhost/myapp');
 
-app.use("/", express.static(__dirname + '/public'));
+mongoose.connection.on("open", function() {
+	console.log('mongo connected');
+});
+
+var UserSchema = new mongoose.Schema({
+	_id: String,
+	username: String,
+	password: String,
+	salt: String,
+	hash: String
+});
+
+var User = mongoose.model('users', UserSchema);
+
+app.configure(function () {
+	app.use(express.cookieParser());
+    app.use(express.session({ secret: 'ytapp' }));
+    app.use(express.bodyParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/public');
+    app.set('view engine', 'jade');
+});
+
+
+function authenticate(name, pass, fn) {
+
+    User.findOne({
+        username: name
+    },
+
+    function (err, user) {
+        if (user) {
+            if (err) return fn(new Error('cannot find user'));
+            hash(pass, user.salt, function (err, hash) {
+                if (err) return fn(err);
+                if (hash == user.hash) return fn(null, user);
+                fn(new Error('invalid password'));
+            });
+        } else {
+            return fn(new Error('cannot find user'));
+        }
+    });
+
+}
+
+function requiredAuthentication(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+}
+
+function userExist(req, res, next) {
+    User.count({
+        username: req.body.username
+    }, function (err, count) {
+        if (count === 0) {
+            next();
+        } else {
+            req.session.error = "User Exist"
+            res.redirect("/signup");
+        }
+    });
+}
+
+http.createServer(app).listen(process.env.PORT || 8080);
+console.log('Server Started Successfully...');
